fc498636a81d8016b62a6ae1b3b352c9026c58a5
jeremy2056 <73949947+jeremy2056@users.noreply.github.com>
2021-01-29 10:26:10 +0100

    Update jeu.py

--- jeu.py
+++ jeu.py
@@ -8,12 +8,19 @@ class player:
         self.position = position
     
 
+class niveau:
 
-class niveau(etage, objets, ennemies):
-    self.etage = etage
-    self.objets = objets
-    self.ennemies = ennemies
+    def __init__(self,id, etage, objets, ennemies):
+        self.id = id
+        self.etage = etage
+        self.objets = objets
+        self.ennemies = ennemies
 
-class etage(salles, couloirs):
-    self.salles = salles
-    self.couloirs = couloirs
+class etage:
+
+    def __init__(self,salles, couloirs):
+        self.salles = salles
+        self.couloirs = couloirs
+    
+salles = [(0,0,4,5), (7,0,3,4), (11,7,5,2)]
+couloirs = [ [(2,5),(2,8),(8,8),(8,4)] , [(4,2),(5,2),(5,1),(6,1)] , [(10,2),(12,2),(12,1),(14,1),(14,6)]]
